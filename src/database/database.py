895c7c77811d0f4aa35cb57fe0e2b14325fc9fc2
dagota12 <chernetdagim@gmail.com>
2024-07-13 17:35:42 +0300

    adding cors

--- src/database/database.py
+++ src/database/database.py
@@ -6,12 +6,13 @@ import pymongo
 from fastapi import HTTPException, status
 load_dotenv() #load enviromental variables
 MONGODB_URI = os.getenv("DB_URI")
+DB_NAME = os.getenv("DB_NAME")
 # print(MONGODB_URI)
 
 async def get_db():
     try:
         client = MongoClient(MONGODB_URI)
-        db = client["mindfull-recovery"]  # Connect to the database
+        db = client[DB_NAME]  # Connect to the database
         return db
     except Exception as e:
         raise HTTPException(
